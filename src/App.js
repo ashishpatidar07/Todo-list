0e245c21a856f0af9bc2e9c64c1b7e3ca8eee7d0
ashishpatidar07 <ashishpatidar4668@gmail.com>
2024-11-09 02:03:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Component/Header";
+import Todos from "./Component/Todos";
+import footer from "./Component/Footer";
+import TodoItem from './Component/TodoItem';
+import AddTodo from "./Component/AddTodo";
+import About from "./Component/About";
+import React, { useEffect, useState } from 'react';
+import Footer from './Component/Footer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  const onDelete = (todo) => {
+    console.log("im onDelete of todo", todo);
+    // let index = todos.indexOf(todo);
+    // todos.splice(index,1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+  }
+
+  const addTodo = (tittle, desc) => {
+    console.log("im adding this todo", tittle, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    } else {
+      let sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      tittle: tittle,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+  const [todos, setTodos] = useState([initTodo]);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header title="my todos list" ></Header>
+        <Routes>
+         
+          <Route path="/" element={
+            <>
+              <AddTodo addTodo={addTodo} />
+              <Todos todos={todos} onDelete={onDelete} />
+            </>
+          } />
+         
+          <Route path="/about" element={<About />} >
+          </Route>
+        </Routes>
+      </Router>
+      <Footer />
+
+
+    </>
   );
 }
 
---
+++ src/Component/About.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>Welcome to Todo List, your personal productivity partner.<br/> This app is designed to simplify your life by helping you organize tasks,<br/> set priorities, and keep track of your progress, all in one easy-to-use platform. <br/>Whether you're managing work projects, personal goals, or daily tasks,<br/> Todo List gives you the tools to stay on top of what matters most.</div>
+  )
+}
